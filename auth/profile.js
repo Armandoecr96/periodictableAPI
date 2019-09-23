2a593943bd57241fe6b8b73a7fdb33aae9063bb0
Armando Carvajal <armandoecr_96@hotmail.com>
2019-09-23 16:27:49 -0500

    change profile to authenticate

--- auth/profile.js
+++ auth/profile.js
@@ -4,15 +4,14 @@ var router = express.Router()
 var con = require('../db/database')
 
 router.get('/profile', function (req, res) {
-  const token = req.body[0].token
+  const token = req.headers.authorization
   var query = `SELECT * FROM (user JOIN profile) JOIN puntuation WHERE token='${token}';`
   con.query(query, function (err, result) {
     if (err) {
       res.send('Failed')
       throw err
     } else {
-      console.log(result)
-      res.send(result)
+      res.send(result[0])
     }
   })
 })
